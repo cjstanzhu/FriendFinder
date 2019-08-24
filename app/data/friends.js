5973dc12f382bb00e6f71eb4126d4c8d1a807dbd
cjstanzhu <cjstanzhu@gmail.com>
2019-08-24 00:24:10 -0400

    revise code

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,18 @@
 let friendsData = [
     {
         name: "Friend Bot",
-        photo: "https://i.pinimg.com/originals/d0/dc/e1/d0dce14f51e5ec88c470abf687e9b214.jpg",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-MEmZFQnK0dmmaXiED0WwruXGL376ZhhETRf1Y57tIlt89lj8",
         scores: ["1", "1", "5", "5", "5", "5", "5", "5", "5", "1"]
+    },
+    {
+        name: "Ahmed",
+        photo: "https://www.birmingham.ac.uk/Images/Staff/profiles/cem/Ahmed,Zubair.jpg",
+        scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+    },
+    {
+        name: "Nemo",
+        photo: "https://s7d9.scene7.com/is/image/DisneyStoreES/412314086166?$sdLargeD$&defaultImage=no%20image-image_uk",
+        scores: ["5", "2", "3", "1", "3", "2", "4", "2", "4", "4"]
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,34 +6,37 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(req, res) {
-        // res.json(true);
-        
-        //insert compatibility logic here
-        
-        let totalDifference = 50; //set initial totalDifference very high
-        let tempDifference = 0;
+        let differencesArray = []; //all the absolute differences
+        let totalDifference = 0;
+        let minDifference = 50; //set an arbitrary high number
         let bestMatches = [];
         
         for (let i = 0; i < friendsData.length; i++) {
             for (let j = 0; j < friendsData[i].scores.length; j++) {
-                tempDifference = tempDifference + Math.abs(req.body.scores[j] - friendsData[i].scores[j]);
+                totalDifference = totalDifference + Math.abs(req.body.scores[j] - friendsData[i].scores[j]);
             };
 
-            if (tempDifference <= totalDifference) {
-                totalDifference = tempDifference;
-                bestMatches.push(friendsData[i]);
+            differencesArray.push(totalDifference);
+
+            totalDifference = 0;
+
+        };
+
+        for (let k = 0; k < differencesArray.length; k++) {
+            if (differencesArray[k] < minDifference) {
+                minDifference = differencesArray[k];
             };
+        };
 
-            tempDifference = 0;
-
+        for (let l = 0; l < differencesArray.length; l++) {
+            if (differencesArray[l] === minDifference) {
+                bestMatches.push(friendsData[l]);
+            };
         };
         
         res.json(bestMatches);
-        // res.json(false); option to add if no matches found or errors occur
         friendsData.push(req.body);
     });
-
-
 
 };
 
